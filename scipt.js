74b7d1091265d49743e43be68909007d44d596dc
PSebastianGarcia <pc.sebastiangarcia@gmail.com>
2023-05-20 14:20:08 -0300

    Script finalizado. completado links index. Ticket finalizado. Style adicional para tickets

---
+++ scipt.js
@@ -0,0 +1,33 @@
+const valueTicket = document.getElementById("value-ticket-showed");
+const resumeButton = document.getElementById("resumen");
+const clearButton = document.getElementById("borrar");
+const TICKET_BASE_PRICE = 200;
+const STUDENT_DESC = 0.80;
+const TRAINEE_DESC = 0.50;
+const JUNIOR_DESC = 0.15;
+const values = [0, 1, 2];
+
+function addValue () {
+    const ticketTypeValue = document.getElementById("option-ticket").value;
+    finalValue (ticketTypeValue);
+}
+
+function clearValue () {    
+    valueTicket.innerHTML = null;
+}
+
+function finalValue (value) {
+    const countTicket = document.getElementById("countInput").value;
+    let price = TICKET_BASE_PRICE;
+    if (value == 1) {
+        price *= STUDENT_DESC;
+    } else if (value == 2) {
+        price *= TRAINEE_DESC;
+    } else {
+        price *= JUNIOR_DESC;
+    };
+    valueTicket.innerHTML = price * countTicket;
+};
+
+resumeButton.onclick = addValue;
+clearButton.onclick = clearValue;
